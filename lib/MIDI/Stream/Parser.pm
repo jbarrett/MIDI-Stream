3c0ed5b32ef95bc9193b31c20a5c747f949df6d4
John Barrett <john@jbrt.org>
2025-03-29 15:41:54 +0000

    Changes to get basic parsing working
    This simplifies some of the parsing at the cost of perhaps going out of
    spec occasionally. May need to be revisited.

--- lib/MIDI/Stream/Parser.pm
+++ lib/MIDI/Stream/Parser.pm
@@ -16,9 +16,9 @@ class MIDI::Stream::Parser {
     use MIDI::Stream::Tables ':all';
 
     field $zero_index_channel  :param = 1;
-    field $concat_multibyte    :param = 1;
-    field $sysex_f0_terminates :param = 1;
     field $sysex_as_string     :param = 1;
+    field $detect_note_off     :param = 1;
+    field $retain_events       :param = 1;
 
     field $mode_14bit :param = 0;
     field $mode_rpn   :param = $mode_14bit;
@@ -32,6 +32,7 @@ class MIDI::Stream::Parser {
 
     field @events;
     field @pending_event;
+    field $events_queued = 0;
 
 
     method attach_callback( $event, $callback ) {
@@ -45,6 +46,7 @@ class MIDI::Stream::Parser {
     method events {
         my @return_events = @events;
         @events = ();
+        $events_queued = 0;
         @return_events;
     }
 
@@ -52,11 +54,12 @@ class MIDI::Stream::Parser {
 
     method _push_event( $event = undef ) {
         state $t = [ gettimeofday ];
-        $event //= \@pending_event;
+        $event //= [ @pending_event ];
+        $events_queued = 1;
 
         # note on with velocity 0 is note off
         $event->[0] = 'note_off'
-            if ( $event->[0] eq 'note_on' && $event->[3] == 0 );
+            if ( $detect_note_off && $event->[0] eq 'note_on' && $event->[3] == 0 );
 
         if ( $event->[0] eq 'sysex' && $sysex_as_string ) {
             $event = [
@@ -66,7 +69,8 @@ class MIDI::Stream::Parser {
             ];
         }
 
-        push @events, $event;
+        push @events, $event if $retain_events;
+
         my $dt = tv_interval( $t );
         $t = [ gettimeofday ];
 
@@ -83,7 +87,6 @@ class MIDI::Stream::Parser {
     method parse( $bytestring ) {
         my @bytes = unpack 'C*', $bytestring;
         my $status;
-        my $ret = 0;
 
         BYTE:
         while ( @bytes ) {
@@ -96,16 +99,12 @@ class MIDI::Stream::Parser {
                 $self->throw( sprintf( "Unsupported status type: 0x%x", $status ) )
                     unless $status_name;
 
-                # Real-Time messages can appear inside other messages
-                if ( is_realtime( $status_name ) ) {
-                    $self->_push_event( [ $status_name ] );
-                    next BYTE;
-                }
-
-                # Non Real-Time single byte statuses
+                # Real-Time messages can appear inside other messages.
+                # Let's just propagate all one-byte statuses as if
+                # they were realtime. This is out of spec, but doesn't
+                # have any weird side-effects I can think of right now.
+                # (Maybe it screws with running status?)
                 if ( message_length( $status ) == 1 ) {
-                    $self->throw( "Non Real-Time message $status_name received while processing $pending_event[0]" )
-                        if @pending_event;
                     $self->_push_event( [ $status_name ] );
                     next BYTE;
                 }
@@ -126,9 +125,6 @@ class MIDI::Stream::Parser {
                     @pending_event = ();
                 }
 
-                $self->throw( "Event $pending_event[0] was not complete before $status_name received" )
-                    if @pending_event;
-
                 @pending_event = ( $status_name );
 
                 # Push channel if required
@@ -141,6 +137,8 @@ class MIDI::Stream::Parser {
                 next BYTE;
             } # end if status byte
 
+            push @pending_event, shift @bytes;
+
             my $message_length = message_length( $pending_event[0] );
             my $remaining = $message_length - @pending_event;
 
@@ -148,28 +146,15 @@ class MIDI::Stream::Parser {
             if ( $message_length && $remaining <= 0 ) {
                 $self->_push_event;
 
-                # Look ahead
-                if ( is_status_byte( $bytes[0] ) ) {
-                    @pending_event = ();
-                }
-                else {
-                    # Probable running status - keep relevant pieces of pending event
-                    @pending_event = has_channel( status_byte( $pending_event[0] ) )
-                        ? @pending_event[ 0, 1 ]
-                        : $pending_event[ 0 ]
-                }
+                # Upcoming messages may include running status -
+                # Status is not retransmitted if it's the same as prev. msg
+                @pending_event = has_channel( status_byte( $pending_event[0] ) )
+                    ? @pending_event[ 0, 1 ]
+                    : $pending_event[ 0 ]
             }
-
-            # Pull up-to the remaining number of bytes for the message,
-            # if we know the length.
-            # If we don't know the length, plop bytes onto the pending event
-            # one-at-a-time ... which may be faster than any grepology
-            # required for look ahead processing.
-            push @pending_event, $message_length
-                ? splice @bytes, 0, $remaining
-                : shift @bytes;
-
         } # end while
+
+        $events_queued;
     }
 
     method encode_events( @events ) {
@@ -182,6 +167,6 @@ class MIDI::Stream::Parser {
 
     method continue { 'continue' }
     method stop { 'stop' }
-
 }
 
+1;
