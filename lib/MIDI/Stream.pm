bbced347f74411631f9364c089ee4eb3b343c701
John Barrett <john@jbrt.org>
2025-11-13 23:03:24 +0000

    MIDI::Stream - mostly docs, perhaps a base class for parser and encoder

---
+++ lib/MIDI/Stream.pm
@@ -0,0 +1,44 @@
+use strict;
+use warnings;
+package MIDI::Stream;
+
+#use MIDI::Stream::Parser;
+#use MIDI::Stream::Encoder;
+
+# ABSTRACT: MIDI bytestream parsing and encoding
+
+use v5.26;
+our @CARP_NOT = (__PACKAGE__);
+
+use Feature::Compat::Class;
+
+class MIDI::Stream {
+    use MIDI::Stream::Tables ();
+
+    method continue { MIDI::Stream::Tables::continue() }
+    method stop { MIDI::Stream::Tables::stop() }
+
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+MIDI::Stream - MIDI bytestream parsing and encoding
+
+=head1 VERSION
+
+version 0.00
+
+=head1 DESCRIPTION
+
+
+
+=cut
+
